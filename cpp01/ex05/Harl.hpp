5594ca1c0136b752c89346b07b1bdec6ed2d97bf
Julia Russell <julrusse@c2r3s6.42lausanne.ch>
2025-12-22 16:15:44 +0100

    05

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,20 @@
+#ifndef HARL_H
+#define HARL_H
+
+#include <iostream>
+#include <string>
+
+class Harl {
+	private:
+		void debug();
+		void info();
+		void warning();
+		void error();
+	public:
+		Harl();
+		~Harl();
+
+		void complain(std::string level);
+};
+
+#endif
