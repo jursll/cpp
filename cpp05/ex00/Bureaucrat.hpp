99e6d347548e4e59a6ac549f2f718185fc574479
Julia Russell <julrusse@c2r3s6.42lausanne.ch>
2026-01-24 13:46:14 +0100

    05

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+	private:
+		std::string const _name;
+		int _grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(Bureaucrat const &src);
+		Bureaucrat& operator=(Bureaucrat const &src);
+		~Bureaucrat();
+
+		void GradeTooHighException() const;
+		void GradeTooLowException() const;
+		std::string getName() const;
+		std::string getGrate() const;
+		int incrementGrade(int grade);
+		int decrementGrade(int grade);
+}
+
+#endif
